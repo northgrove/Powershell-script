933d054014e01680d001f919f6ad9ca89267bc3b
Kjetil Nordlund <kjnordlu@microsoft.com>
2019-12-22 13:16:08 +0100

    readme updates

--- run_all.ps1
+++
@@ -1,7 +0,0 @@
-
-
-.\AzureATP\Account_enumeration_reconnaissance.ps1
-.\AzureATP\LDAP_Bind_password_spray.ps1
-.\Office365ATP\Send_email_Safelinks.ps1
-.\Office365ATP\Send_email_UserImpersionating.ps1
-
